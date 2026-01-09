e6d422b7b1c5be6c724730c3e47fbf69067da566
niclashogh <113596227+niclashogh@users.noreply.github.com>
2026-01-09 15:14:50 +0100

    .

---
+++ eft_app_guide/Models/Rewards/Reward.cs
@@ -0,0 +1,20 @@
+﻿using eft_app_guide.Models.Rewards.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eft_app_guide.Models.Rewards
+{
+    public class Reward
+    {
+        #region Keys
+        [Key] public int Id { get; init; }
+        #endregion
+
+        [Required] public RewardTypes Type { get; set; }
+        [Required] public 
+    }
+}
