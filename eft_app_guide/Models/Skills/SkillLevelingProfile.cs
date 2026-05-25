bf57975da0e43b2d4ea4021ae1264ec3af6f35ee
niclashogh <113596227+niclashogh@users.noreply.github.com>
2026-05-25 14:30:26 +0200

    .

--- eft_app_guide/Models/Skills/SkillLevelingProfile.cs
+++ eft_app_guide/Models/Skills/SkillLevelingProfile.cs
@@ -2,7 +2,7 @@
 
 namespace eft_app_guide.Models.Skills
 {
-    public class SkillLevelingProfile // TODO
+    public class SkillLevelingProfile // TODO - Add InGame Leveling (129%, 100% < 100%) in SkillLevelRate(Percent Rate, int Cooldown, int UsesBeforeCooldown) to associate Workstation bonuses (Also rename classes to Workstation).
     {
         #region Keys
         [Key] public int Id { get; init; }
